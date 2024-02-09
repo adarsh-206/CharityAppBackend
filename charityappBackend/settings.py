3efe8d5911ad4082d4eb67bc62df19101e55af40
adarsh-206 <adarshdhote8624@gmail.com>
2024-02-09 10:47:23 +0530

    image format changes

--- charityappBackend/settings.py
+++ charityappBackend/settings.py
@@ -26,8 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
-    'drf_extra_fields'
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
